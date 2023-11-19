4e1e110f8030334abe8dd6bdebc422e092fc63a3
Xiaoyan Bai <72651795+Elena-Baixy@users.noreply.github.com>
2023-11-19 15:54:05 -0500

    Update test_gpt.py

--- tf/test_gpt.py
+++ tf/test_gpt.py
@@ -7,14 +7,14 @@ from datasets import load_dataset
 
 num_example = 2 #change the number of example here
 sst2 = load_dataset("sst2",split = "validation") #load the dataset here
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2-large") #change the model
-model = GPT2ForSequenceClassification.from_pretrained("gpt2-large", activation_function="relu") #change the model
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2-medium") #change the model
+model = GPT2ForSequenceClassification.from_pretrained("gpt2-medium") #change the model
 print(model)
 
 i = 0
 for sentence in sst2["sentence"][:num_example]: 
   inputs = tokenizer(sentence, return_tensors = "pt")
-  outputs = model(**inputs, output_hidden_states = True,use_cache = True)
+  outputs = model(**inputs, output_hidden_states = True) #,use_cache = True
   i += 1
   print ("======= example " + str(i) + "========")
 
